000154f246d6a959ae24196593a9b34f54178ab5
thruma17-midon <210378036+thruma17@users.noreply.github.com>
2026-03-09 16:26:05 +0100

    Create Book constructor

---
+++ script.js
@@ -0,0 +1,16 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  if (!new.target) {
+    throw Error("You must use the 'new' operator to call the constructor");
+  }
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.id = crypto.randomUUID();
+}
+
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+}
